06442b79e8f49555d83337849b85b60b7f759e17
yuokada <callistoiv+git@gmail.com>
2017-05-14 14:59:08 +0900

    Add debu Request/Response flag

--- server.go
+++ server.go
@@ -15,6 +15,7 @@ import (
 
 type transport struct {
 	http.RoundTripper
+	DebugRequest, DebugResponse bool
 }
 
 func debugPrint(dumps string) {
@@ -24,11 +25,13 @@ func debugPrint(dumps string) {
 }
 
 func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	dumps, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		return nil, err
+	if t.DebugRequest {
+		dumps, err := httputil.DumpRequest(req, true)
+		if err != nil {
+			return nil, err
+		}
+		debugPrint(string(dumps))
 	}
-	debugPrint(string(dumps))
 
 	resp, err = t.RoundTripper.RoundTrip(req)
 	if err != nil {
@@ -45,19 +48,24 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 	resp.ContentLength = int64(len(b))
 	resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
 	resp.Body = body
-	_body, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		return nil, err
+	if t.DebugResponse {
+		_body, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			return nil, err
+		}
+		debugPrint(string(_body))
 	}
-	debugPrint(string(_body))
 	return resp, nil
 }
 
 func main() {
 	var dst string
 	var port int
+	var debugReq, debugResp bool
 	flag.StringVar(&dst, "dst", "http://localhost:8080", "proxy destination")
 	flag.IntVar(&port, "port", 8081, "listen port")
+	flag.BoolVar(&debugReq, "drequest", true, "set true if you debug Request")
+	flag.BoolVar(&debugResp, "dresponse", true, "set true if you debug Response")
 	flag.Parse()
 	u, err := url.Parse(dst)
 	if err != nil {
@@ -67,7 +75,11 @@ func main() {
 
 	// see: http://stackoverflow.com/questions/31535569/golang-how-to-read-response-body-of-reverseproxy
 	proxy := httputil.NewSingleHostReverseProxy(u)
-	proxy.Transport = &transport{http.DefaultTransport}
+	proxy.Transport = &transport{
+		http.DefaultTransport,
+		debugReq,
+		debugResp,
+	}
 	http.Handle("/", proxy)
 	err = http.ListenAndServe(fmt.Sprintf(":%d", port), proxy)
 	if err != nil {
